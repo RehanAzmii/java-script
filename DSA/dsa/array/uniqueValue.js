c4b9c1b631a93306f1f03fce2294b5c262c835dc
Rehan <rehanmakhdoomi93@gmail.com>
2023-10-12 15:22:49 +0530

    array

--- DSA/dsa/array/uniqueValue.js
+++ DSA/dsa/array/uniqueValue.js
@@ -7,11 +7,45 @@
 // const uniqueValue = Array.from(unique);
 // console.log(uniqueValue);
 
-const array = [1, 2, 2, 3, 4, 4, 5, 6, 6];
-let uniqueValue = [];
-for (let i = 0; i < array.length; i++) {
-  if (!uniqueValue.includes(array[i])) {
-    uniqueValue.push(array[i]);
+// const array = [1, 2, 2, 3, 4, 4, 5, 6, 6];
+// let uniqueValue = [];
+// for (let i = 0; i < array.length; i++) {
+//   if (!uniqueValue.includes(array[i])) {
+//     uniqueValue.push(array[i]);
+//   }
+// }
+// console.log(uniqueValue);
+
+// find duplicate value in the array
+// const array = [1, 2, 2, 3, 4, 4, 5, 6, 6];
+// const duplicates = [];
+
+// for (let i = 0; i < array.length; i++) {
+//   for (let j = i + 1; j < array.length; j++) {
+//     if (array[i] === array[j] && !duplicates.includes(array[i])) {
+//       duplicates.push(array[i]);
+//     }
+//   }
+// }
+
+// console.log("Duplicate elements in the array:", duplicates);
+
+function firstRepeatingElement(arr) {
+  // Nested loop to check for repeating elements
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      // If a repeating element is found, return its index
+      if (arr[i] === arr[j]) {
+        return i;
+      }
+    }
   }
+  // If no repeating element is found, return -1
+  return -1;
 }
-console.log(uniqueValue);
+
+// Driver code
+const arr = [10, 5, 3, 4, 3, 5, 6];
+// Finding the index of first repeating element
+const index = firstRepeatingElement(arr);
+console.log(index);
