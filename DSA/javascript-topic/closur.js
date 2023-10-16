e7072e3690abcd63ff65837a27c56671d31087e6
Rehan <rehanmakhdoomi93@gmail.com>
2023-10-16 11:07:27 +0530

    dsa

--- DSA/javascript-topic/closur.js
+++ DSA/javascript-topic/closur.js
@@ -1,27 +1,40 @@
-const outerFunction = () => {
-  const outerVairiable = "outer variable";
-  const innerFunc = () => {
-    console.log(outerVairiable);
-  };
-  return innerFunc;
-};
-const res = outerFunction();
-res();
+// const outerFunction = () => {
+//   const outerVairiable = "outer variable";
+//   const innerFunc = () => {
+//     console.log(outerVairiable);
+//   };
+//   return innerFunc;
+// };
+// const res = outerFunction();
+// res();
 
-// high order function
+// // high order function
 
-const callBackFunc = () => {
-  console.log("this is a call back functio");
-};
-const highorederFunction = (func) => {
-  console.log("this is a high order function");
-  func();
-};
+// const callBackFunc = () => {
+//   console.log("this is a call back functio");
+// };
+// const highorederFunction = (func) => {
+//   console.log("this is a high order function");
+//   func();
+// };
 
-highorederFunction(callBackFunc);
+// highorederFunction(callBackFunc);
 
-// function currying
+// // function currying
 
-const sum = (a) => (b) => (c) => (d) => console.log(a + (b % c) + d);
+// const sum = (a) => (b) => (c) => (d) => console.log(a + (b % c) + d);
 
-sum(1)(2)(3)(4);
+// sum(1)(2)(3)(4);
+
+// function ahsan (){
+
+// }
+
+// export  let sana = function(){
+
+// }
+// sana()
+
+// let rehan = () =>{
+
+// }
